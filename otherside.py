159c231a08558605f89a67bb4beb4d46be87839c
Lee Goldin Zawieja <lewleegz@gmail.com>
2025-05-01 17:04:12 +0200

    Update otherside.py

--- otherside.py
+++ otherside.py
@@ -7,11 +7,13 @@ st.write("Click the button below to get your antipode.")
 
 location2 = streamlit_geolocation()
 location =str(location2)
-
-location = location.replace("{", "").replace("}", "").replace("'", "")
-parts = location.split(",")
-st.session_state.latitud = float(parts[0].split(":")[1].strip())
-st.session_state.longitud = float(parts[1].split(":")[1].strip())
+try:
+    location = location.replace("{", "").replace("}", "").replace("'", "")
+    parts = location.split(",")
+    st.session_state.latitud = float(parts[0].split(":")[1].strip())
+    st.session_state.longitud = float(parts[1].split(":")[1].strip())
+except ValueError:
+    st.error("")
 
 if st.session_state.latitud and st.session_state.longitud:
     try:
@@ -19,7 +21,7 @@ if st.session_state.latitud and st.session_state.longitud:
         longitud = float(st.session_state.longitud)
         latitud = latitud * -1
         longitud = (longitud + 180) if longitud < 0 else (longitud - 180)
-        st.write("Your otherside are:", latitud, longitud)
+        st.write("Your otherside is:", latitud, longitud)
         st.link_button("Go to maps", f"https://www.google.com/maps/search/?api=1&query={latitud},{longitud}")
     except ValueError:
         st.error("Location not found")
